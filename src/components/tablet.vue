c2ca648a2e5211ed282aadc9981e2026f09d9b6e
Roï Eielts <103494921+Roi-Eielts@users.noreply.github.com>
2024-09-04 18:38:59 +0200

    fixed images

--- src/components/tablet.vue
+++ src/components/tablet.vue
@@ -1,13 +1,19 @@
 <script>
 export default {
   props: ['image'],
+  computed: {
+    fullImagePath() {
+      return import.meta.env.BASE_URL + this.image;
+    }
+  }
 }
 </script>
 
 <template>
   <div class="tablet w-100 rounded-4 bg-secondary p-1">
-    <div class="screen bg-dark rounded-4 d-flex h-100 pe-2 pt-auto pb-auto justify-content-end" :style="{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
-        <!-- <img :src="image" class="w-100 rounded"/> -->
+    <div class="screen bg-dark rounded-4 d-flex h-100 pe-2 pt-auto pb-auto justify-content-end" 
+         :style="{ backgroundImage: `url(${fullImagePath})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
+        <!-- <img :src="fullImagePath" class="w-100 rounded"/> -->
         <div class="vr float-end text-primary border-start border-5 h-50 rounded-5 my-auto border-dark"></div>
     </div>
   </div>
